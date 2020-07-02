96d4d30630de68b8492a99aadf9f5460e01e4e40
Bishal <bishal1banerjee@gmail.com>
2020-07-02 23:05:37 +0530

    Added header:

--- chatter/client.py
+++ chatter/client.py
@@ -3,6 +3,13 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 5050))
 
-msg = s.recv(1024)
-print(msg.decode("utf8"))
+fullmessage = ""
 
+while True:
+    msg = s.recv(8)
+    if len(msg) <= 0:
+        break
+    else:
+        fullmessage += msg.decode("utf8")
+
+print(fullmessage)
--- chatter/server.py
+++ chatter/server.py
@@ -1,5 +1,7 @@
 import socket
 
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 5050))
 s.listen(5)   # Queue of 5.  5 connection maximum
@@ -7,5 +9,9 @@ s.listen(5)   # Queue of 5.  5 connection maximum
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
+
+    message = "Welcome to the server!!"
+    message = f"{len(message):<10}" + message
+
     clientsocket.send(bytes("Welcome to the server", "utf8"))
-    
+    clientsocket.close()
