d987d34a7cbc3ec183b35d68795639549682abf9
Bishal <bishal1banerjee@gmail.com>
2020-07-03 11:23:27 +0530

    Working connection

--- chatter/client.py
+++ chatter/client.py
@@ -3,13 +3,23 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 5050))
 
-fullmessage = ""
+HEADERSIZE = 10
 
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    else:
+    fullmessage = ""
+    newmessage = True
+    while True:
+        msg = s.recv(16)
+        if newmessage:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            newmessage = False
+
         fullmessage += msg.decode("utf8")
 
-print(fullmessage)
+        if len(fullmessage) - HEADERSIZE == msglen:
+            print("full msg received")
+            print(fullmessage[HEADERSIZE:])
+            newmessage = True
+            fullmessage = ""
+    print(fullmessage)
--- chatter/server.py
+++ chatter/server.py
@@ -1,17 +1,23 @@
 import socket
+import time
 
-
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 5050))
-s.listen(5)   # Queue of 5.  5 connection maximum
+s.listen(5)  # Queue of 5.  5 connection maximum
+print("Server is Running...")
 
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
 
     message = "Welcome to the server!!"
-    message = f"{len(message):<10}" + message
+    message = f"{len(message):<{HEADERSIZE}}" + message
 
-    clientsocket.send(bytes("Welcome to the server", "utf8"))
-    clientsocket.close()
+    clientsocket.send(bytes(message, "utf8"))
+    while True:
+        time.sleep(3)
+        message = f"The time is: {time.time()}"
+        message = f"{len(message):<{HEADERSIZE}}" + message
+        clientsocket.send(bytes(message, "utf8"))
